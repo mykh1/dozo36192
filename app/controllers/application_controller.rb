255b01e0236992e93ffaed78e35753a36fc6856a
mykh1 <neige391124@yahoo.co.jp>
2021-11-04 21:26:39 +0900

    ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number,:last_name, :first_name, :last_name_reading, :first_name_reading, :nickname])
+  end
 end
