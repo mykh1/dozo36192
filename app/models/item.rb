de139b1fa986bcb556f8ca3fb6a1c30c05d11ec2
mykh1 <neige391124@yahoo.co.jp>
2021-11-21 17:19:07 +0900

    画像投稿

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_description
+  end
+
+  
 
 end
