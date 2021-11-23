fa6e8d5b96d66db0904397ca501f2e64bd252f3b
mykh1 <neige391124@yahoo.co.jp>
2021-11-23 16:59:32 +0900

    item単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence } # 商品の説明
+    category_id { 1 } # カテゴリー
+    condition_id { 1 } # 商品の状態
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    employee_number { 'A123456' }
+    employee_number { 'A111222' }
     last_name { '山田' }
     first_name { '太郎' }
     last_name_reading { 'ヤマダ' }
     first_name_reading { 'タロウ' }
-    nickname { 'たろう' }
+    nickname { 'たろ' }
     email  {Faker::Internet.free_email}
     password { 'abc123' }
     password_confirmation { password }
