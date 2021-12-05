7aa4bd366309804ea9faf1720145c044234230d3
mykh1 <neige391124@yahoo.co.jp>
2021-12-05 17:42:19 +0900

    チェックボックス ３

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,12 @@
 class OrdersController < ApplicationController
 
   def create 
-    @item = Item.find(params[:item_id])
-    @user = User.find(@item.user_id)
-    # @item = Item.find(params[:id])
-    # @user = User.find_by(id: @item.user_id)
     @order = current_user.orders.build(order_params)
     if @order.save
       redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
+      @user = User.find(@item.user_id)
       render "items/show"
     end
   end
@@ -16,8 +14,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    # params.require(:order).permit(:item_id, :accept)
-    params.permit(:item_id, :accept)
+    params.require(:order).permit(:item_id, :accept)
   end
 
 end
