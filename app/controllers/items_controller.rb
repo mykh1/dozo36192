c319b5c2a7609923a6165b3593318f5bd8bf2e58
mykh1 <neige391124@yahoo.co.jp>
2021-12-06 21:16:28 +0900

    編集・削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :new, :create, :show]
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -18,15 +21,42 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @user = User.find_by(id: @item.user_id)
     @order = Order.new
+  end
+
+  def edit
+    if @item.order != nil
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless current_user.id == @item.user_id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,9 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:item_id, :accept)
+    params.require(:order).permit(:item_id, :accept) #htmlに<%= f.hidden_field :item_id, value: @item.id %>を書く場合の書き方
+    # params.require(:order).permit(:item_id, :accept).merge(item_id: params[:item_id]) ⬅︎ htmlに<%= f.hidden_field を書かないで、mergeで書いた場合
+
   end
 
 end
